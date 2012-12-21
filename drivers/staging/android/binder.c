fffbf66da3599c3b718eabf8a45ad6f300fc1032
Cruz Julian Bishop <cruzjbishop@gmail.com>
2012-12-22 09:00:45 +1000

    staging: android: Fix two checkpatch issues in binder.c
    This fixes two instances of
    "static const char * array should probably be static const char * const"
    
    I have seen other commits doing this in other files, so I am
    assuming it should be done here as well.
    
    Please tell me if this is wrong :)
    
    Signed-off-by: Cruz Julian Bishop <cruzjbishop@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: stratosk <stratosk@semaphore.gr>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -3379,7 +3379,7 @@ static void print_binder_proc(struct seq_file *m,
 		m->count = start_pos;
 }
 
-static const char *binder_return_strings[] = {
+static const char * const binder_return_strings[] = {
 	"BR_ERROR",
 	"BR_OK",
 	"BR_TRANSACTION",
@@ -3400,7 +3400,7 @@ static const char *binder_return_strings[] = {
 	"BR_FAILED_REPLY"
 };
 
-static const char *binder_command_strings[] = {
+static const char * const binder_command_strings[] = {
 	"BC_TRANSACTION",
 	"BC_REPLY",
 	"BC_ACQUIRE_RESULT",
@@ -3420,7 +3420,7 @@ static const char *binder_command_strings[] = {
 	"BC_DEAD_BINDER_DONE"
 };
 
-static const char *binder_objstat_strings[] = {
+static const char * const binder_objstat_strings[] = {
 	"proc",
 	"thread",
 	"node",
