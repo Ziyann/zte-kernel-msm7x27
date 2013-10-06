5693affff55ccab4f1512a41469d75a9abdb3927
Ziyann <jaraidaniel@gmail.com>
2013-10-06 15:06:42 +0200

    Rename The wifi driver didn't like this, so we need to ignore version magic (treat it as a warning instead an error).

--- kernel/module.c
+++ kernel/module.c
@@ -2230,10 +2230,8 @@ static noinline struct module *load_module(void __user *umod,
 		if (err)
 			goto free_hdr;
 	} else if (!same_magic(modmagic, vermagic, versindex)) {
-		printk(KERN_ERR "%s: version magic '%s' should be '%s'\n",
+		printk(KERN_WARNING "%s: version magic '%s' should be '%s'\n",
 		       mod->name, modmagic, vermagic);
-		err = -ENOEXEC;
-		goto free_hdr;
 	}
 
 	staging = get_modinfo(sechdrs, infoindex, "staging");
